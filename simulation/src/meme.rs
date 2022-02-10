61ecac7869512547dfdebd76b8a12942413fc6e4
On0n0k1 <stiltztinkerstein@gmail.com>
2022-02-10 16:14:14 -0300

    Reverting simulation back to the previous state

--- simulation/src/meme.rs
+++ simulation/src/meme.rs
@@ -1,105 +1,57 @@
 use std::fmt::Debug;
+
 use near_sdk::near_bindgen;
-use utils::Category;
-use meme::Vote;
 
+#[near_bindgen]
+pub struct Meme {}
 
-#[ext_contract(meme)]
-pub trait MemeExtContract{
+#[near_bindgen]
+impl Meme {
     // ------------------------------------------------------------------------
     // initialization
     // ------------------------------------------------------------------------
 
-    pub fn new(title: &str, data: &str, category: Category);
+    pub fn init(title: &str, data: &str, category: u8) {}
 
-    pub fn get_meme();
+    // pub fn get_meme() -> Meme {}
+    pub fn get_meme() {}
 
-    pub fn vote(value: i8);
+    // ------------------------------------------------------------------------
+    // voting
+    // ------------------------------------------------------------------------
 
-    pub fn batch_vote(value: i8, is_batch: bool);
+    pub fn vote(value: i8) {}
 
-    #[result_serializer(borsh)]
-    pub fn get_recent_votes() -> Vec<Vote>;
+    pub fn batch_vote(value: i8, is_batch: bool) {}
 
-    pub fn get_vote_score() -> i32;
+    // pub fn get_recent_votes() -> Vec<Vote> {}
+    pub fn get_recent_votes() {}
 
-    // ----------------------------------------------------------------------------
-    // Comments
-    // ----------------------------------------------------------------------------
+    // pub fn get_vote_score() -> i32 {}
+    pub fn get_vote_score() {}
 
-    pub fn add_comment(text: &str);
+    // ------------------------------------------------------------------------
+    // comments
+    // ------------------------------------------------------------------------
 
-    #[result_serializer(borsh)]
-    pub fn get_recent_comments() -> Vec<Comment>;
+    pub fn add_comment(text: &str) {}
 
-    // ----------------------------------------------------------------------------
-    // Donations
-    // ----------------------------------------------------------------------------
+    // pub fn get_recent_comments() -> Array<Comment> {}
+    pub fn get_recent_comments() {}
 
-    #[payable]
-    pub fn donate();
+    // ------------------------------------------------------------------------
+    // donations
+    // ------------------------------------------------------------------------
 
-    pub fn get_donations_total() -> U128;
+    pub fn donate() {}
 
-    #[result_serializer(borsh)]
-    pub fn get_recent_donations() -> Vec<Donation>;
+    // pub fn get_donations_total() -> U128 {}
+    pub fn get_donations_total() {}
 
-    pub fn release_donations(account: &str);
+    // pub fn get_recent_donations(): Array<Donation> {
+    pub fn get_recent_donations() {}
 
-    pub fn on_donations_released();
+    pub fn release_donations(account: &str) {}
+
+    pub fn on_donations_released() {}
 }
-
-
-// #[near_bindgen]
-// pub struct Meme {}
-
-// #[near_bindgen]
-// impl Meme {
-//     // ------------------------------------------------------------------------
-//     // initialization
-//     // ------------------------------------------------------------------------
-
-//     pub fn new(title: &str, data: &str, category: u8) {}
-
-//     // pub fn get_meme() -> Meme {}
-//     pub fn get_meme() {}
-
-//     // ------------------------------------------------------------------------
-//     // voting
-//     // ------------------------------------------------------------------------
-
-//     pub fn vote(value: i8) {}
-
-//     pub fn batch_vote(value: i8, is_batch: bool) {}
-
-//     // pub fn get_recent_votes() -> Vec<Vote> {}
-//     pub fn get_recent_votes() {}
-
-//     // pub fn get_vote_score() -> i32 {}
-//     pub fn get_vote_score() {}
-
-//     // ------------------------------------------------------------------------
-//     // comments
-//     // ------------------------------------------------------------------------
-
-//     pub fn add_comment(text: &str) {}
-
-//     // pub fn get_recent_comments() -> Array<Comment> {}
-//     pub fn get_recent_comments() {}
-
-//     // ------------------------------------------------------------------------
-//     // donations
-//     // ------------------------------------------------------------------------
-
-//     pub fn donate() {}
-
-//     // pub fn get_donations_total() -> U128 {}
-//     pub fn get_donations_total() {}
-
-//     // pub fn get_recent_donations(): Array<Donation> {
-//     pub fn get_recent_donations() {}
-
-//     pub fn release_donations(account: &str) {}
-
-//     pub fn on_donations_released() {}
-// }
--- simulation/src/museum.rs
+++ simulation/src/museum.rs
@@ -1,103 +1,54 @@
 use near_sdk::json_types::U128;
-use near_sdk::{near_bindgen, result_serializer};
+use near_sdk::near_bindgen;
 
+#[near_bindgen]
+pub struct Museum {}
 
-use museum::Museum;
+#[near_bindgen]
+impl Museum {
+    // ------------------------------------------------------------------------
+    // initialization
+    // ------------------------------------------------------------------------
 
+    pub fn init(name: &str, owners: Vec<&str>) {}
 
-#[ext_contract(museum)]
-pub trait MuseumExtContract{
-    pub fn new(
-        name: &str, 
-        owners: Vec<String>,
-    );
+    // pub fn get_museum() -> Museum
+    pub fn get_museum() {}
 
-    #[result_serializer(borsh)]
-    pub fn get_museum() -> Museum;
+    // ------------------------------------------------------------------------
+    // self-service methods
+    // ------------------------------------------------------------------------
 
-    #[result_serializer(borsh)]
-    pub fn get_owner_list() -> Vec<String>;
+    // pub fn get_owner_list() -> AccountId[]
+    pub fn get_owner_list() {}
 
-    #[result_serializer(borsh)]
-    pub fn get_meme_list() -> Vec<String>;
+    // pub fn get_meme_list() -> AccountId[]
+    pub fn get_meme_list() {}
 
-    pub fn get_meme_count() -> u32;
+    // pub fn get_meme_count() -> u32
+    pub fn get_meme_count() {}
 
-    pub fn add_myself_as_contributor();
+    pub fn add_myself_as_contributor() {}
 
-    pub fn remove_myself_as_contributor();
+    pub fn remove_myself_as_contributor() {}
 
-    #[result_serializer(borsh)]
-    pub fn add_meme(meme: &str, title: &str, data: &str, category: Category);
-
+    pub fn add_meme(meme: &str, title: &str, data: &str, category: u8) {}
 
     // ------------------------------------------------------------------------
     // owner methods
     // ------------------------------------------------------------------------
 
-    pub fn add_contributor(account: &str);
+    pub fn add_contributor(account: &str) {}
 
-    pub fn remove_contributor(account: &str);
+    pub fn remove_contributor(account: &str) {}
 
-    pub fn add_owner(account: &str);
+    pub fn add_owner(account: &str) {}
 
-    pub fn remove_owner(account: &str);
+    pub fn remove_owner(account: &str) {}
 
-    pub fn remove_meme(meme: &str);
+    pub fn remove_meme(meme: &str) {}
 
-    pub fn on_meme_removed(meme: &str);
+    pub fn on_meme_removed(meme: &str) {}
+
+    pub fn museum_to_meme_proxy(meme: &str, view_fn: &str) {}
 }
-
-
-
-// #[near_bindgen]
-// pub struct Museum {}
-
-// #[near_bindgen]
-// impl Museum {
-//     // ------------------------------------------------------------------------
-//     // initialization
-//     // ------------------------------------------------------------------------
-
-//     pub fn new(name: &str, owners: Vec<&str>) {}
-
-//     // pub fn get_museum() -> Museum
-//     pub fn get_museum() {}
-
-//     // ------------------------------------------------------------------------
-//     // self-service methods
-//     // ------------------------------------------------------------------------
-
-//     // pub fn get_owner_list() -> AccountId[]
-//     pub fn get_owner_list() {}
-
-//     // pub fn get_meme_list() -> AccountId[]
-//     pub fn get_meme_list() {}
-
-//     // pub fn get_meme_count() -> u32
-//     pub fn get_meme_count() {}
-
-//     pub fn add_myself_as_contributor() {}
-
-//     pub fn remove_myself_as_contributor() {}
-
-//     pub fn add_meme(meme: &str, title: &str, data: &str, category: u8) {}
-
-//     // ------------------------------------------------------------------------
-//     // owner methods
-//     // ------------------------------------------------------------------------
-
-//     pub fn add_contributor(account: &str) {}
-
-//     pub fn remove_contributor(account: &str) {}
-
-//     pub fn add_owner(account: &str) {}
-
-//     pub fn remove_owner(account: &str) {}
-
-//     pub fn remove_meme(meme: &str) {}
-
-//     pub fn on_meme_removed(meme: &str) {}
-
-//     pub fn museum_to_meme_proxy(meme: &str, view_fn: &str) {}
-// }
